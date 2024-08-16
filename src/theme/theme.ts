8211b8f5becf285f21715fa6d881f693019d90d9
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-08-16 15:29:00 +0500

    Add comments in theme for font generic usage in app

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,24 +3,28 @@ import { createTheme, Theme } from "@mui/material/styles";
 const theme: Theme = createTheme({
   typography: {
     fontFamily: '"Poppins", "Roboto", "Helvetica", "Arial", sans-serif',
+    // Main heading
     h3: {
       fontWeight: 700,
       fontSize: "30px",
       lineHeight: "45px",
       color: "#000000",
     },
+    // Smaller heading
     h4: {
       fontWeight: 500,
       fontSize: "16px",
       lineHeight: "24px",
       color: "#000000",
     },
+    // Main paragraph
     body1: {
       fontWeight: 400,
       fontSize: "18px",
       lineHeight: "28px",
       color: "#8692A6",
     },
+    // Smaller paragraph
     body2: {
       fontWeight: 400,
       fontSize: "14px",
