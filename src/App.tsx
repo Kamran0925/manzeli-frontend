c0dfb91730be966679c791869dc9020a8300a16d
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-08-27 20:29:56 +0500

    Add global routing for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
 import { FormProvider } from "./context/FormContext";
+import { Route, Routes } from "react-router-dom";
 import Registration from "./components/registration/registration";
+import Login from "./components/login/Login";
+import ForgotPassword from "./components/forgot-password/Forgot-Password";
+import ResetPassword from "./components/reset-password/reset-password";
 
 function App() {
   return (
     <FormProvider>
-      <Registration />
+      <Routes>
+        <Route path="/" Component={Registration} />
+        <Route path="/login" Component={Login} />
+        <Route path="/forgot-password" Component={ForgotPassword} />
+        <Route path="/reset-password" Component={ResetPassword} />
+      </Routes>
     </FormProvider>
   );
 }
