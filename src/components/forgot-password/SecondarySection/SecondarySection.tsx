900032ee3f2f123779a9dd3472f7ff1ba9e86718
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-08-27 19:05:26 +0500

    Add secondary section for forgot password

---
+++ src/components/forgot-password/SecondarySection/SecondarySection.tsx
@@ -0,0 +1,118 @@
+import { useState } from "react";
+import { Typography, Box, Link, Button } from "@mui/material";
+import InputField from "../../shared/InputField/InputField";
+
+import styles from "./SecondarySection.module.css";
+
+interface SecondarySectionProps {
+  onNext: () => void;
+}
+
+const SecondarySection: React.FC<SecondarySectionProps> = ({ onNext }) => {
+  const [name, setName] = useState<string>("");
+
+  const [isButtonDisabled, setIsButtonDisabled] = useState<boolean>(false);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (value.length < 8) {
+      setIsButtonDisabled(true);
+    }
+    setName(value);
+  };
+
+  const handleSubmit = () => {
+    onNext();
+  };
+
+  return (
+    <Box component="section" className={styles.box1}>
+      <Box className={styles.box2}>
+        <Typography
+          variant="h4"
+          sx={{
+            color: "#040308",
+            fontFamily: "Poppins, sans-serif",
+            fontSize: "32px",
+            fontStyle: "normal",
+            fontWeight: 700,
+            lineHeight: "normal",
+          }}
+        >
+          Forgot Password
+        </Typography>
+
+        <Typography variant="h4" fontWeight={400}>
+          Enter the Username you used to create your account so we can send you
+          instructions on how to reset your password.
+        </Typography>
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            marginTop: "100px",
+          }}
+        >
+          <InputField
+            type="text"
+            name="username"
+            placeholder="Username"
+            value={name}
+            errorMessage={""}
+            handleChange={handleChange}
+          />
+
+          <Button
+            variant="contained"
+            sx={{
+              margin: "30px 0px",
+              padding: "15px 20px",
+              height: "54px",
+              width: "100%",
+              fontSize: "16px",
+              borderRadius: "40px",
+              backgroundColor: "#001283",
+              color: "#fff",
+              textTransform: "none",
+              "&:hover": {
+                cursor: "pointer",
+                backgroundColor: "#000a4e",
+              },
+            }}
+            onClick={handleSubmit}
+            disabled={isButtonDisabled}
+          >
+            Send
+          </Button>
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              width: "100%",
+              marginTop: "15px",
+              fontWeight: 600,
+            }}
+          >
+            <Link
+              href="/forgot-password"
+              sx={{
+                textDecoration: "none",
+                color: "#000",
+                "&:hover": {
+                  textDecoration: "underline",
+                },
+              }}
+            >
+              Back to Login
+            </Link>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default SecondarySection;
