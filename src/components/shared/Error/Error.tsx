7ff00846e9d9ea1372bf72c0cae64ad1110bbbcb
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-08-28 21:29:07 +0500

    Add generic error component for registration and login pages

--- src/components/shared/Error/Error.tsx
+++ src/components/shared/Error/Error.tsx
@@ -11,15 +11,18 @@ const Error: React.FC<ErrorProps> = ({ messages }) => {
       sx={{
         display: "flex",
         flexDirection: "column",
-        padding: "14px",
+        boxSizing: "border-box",
+        padding: "14px 20px",
         gap: "8px",
         maxWidth: "554px",
-        bgcolor: "rgba(59, 76, 184, 0.11)",
-        borderRadius: "20px",
+        bgcolor: "rgba(232, 0, 0, 0.08)",
+        borderRadius: "40px",
+        marginTop: "12px",
+        width: "100%",
       }}
     >
       {messages.map((msg, index) => (
-        <Typography key={index} variant="body2" color="#6D6D6D">
+        <Typography key={index} variant="body2" color="#E80000">
           {msg}
         </Typography>
       ))}
