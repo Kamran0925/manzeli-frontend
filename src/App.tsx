0fe1f9beb2b5d977915d8aae47bc8aa15153ef80
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-08-28 16:01:00 +0500

    Add routes for authentication component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { FormProvider } from "./context/FormContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Authentication from "./components/authentication/authentication";
 
 function App() {
   return (
     <FormProvider>
-      <Authentication />
+      <Router>
+        <Routes>
+          <Route path="/*" element={<Authentication />} />
+        </Routes>
+      </Router>
     </FormProvider>
   );
 }
--- src/components/authentication/authentication.tsx
+++ src/components/authentication/authentication.tsx
@@ -1,12 +1,14 @@
 import { Container, Grid } from "@mui/material";
-
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import PrimarySection from "./PrimarySection/PrimarySection";
 import Login from "./Login/Login";
 import Registration from "./Registration/Registration";
 import styles from "./authentication.module.css";
 
 const Authentication = () => {
-  const isLogin = false;
+  const location = useLocation();
+  const isLoginRoute = location.pathname === "/login";
+
   return (
     <Container disableGutters maxWidth={false}>
       <Grid container>
@@ -15,12 +17,16 @@ const Authentication = () => {
           xs={12}
           sm={12}
           md={4}
-          className={`${styles.leftGrid} ${isLogin ? styles.loginImage : ""}`}
+          className={isLoginRoute ? styles.loginImage : styles.leftGrid}
         >
-          {isLogin && <PrimarySection />}
+          {!isLoginRoute && <PrimarySection />}
         </Grid>
         <Grid item xs={12} sm={12} md={8} className={styles.rightGrid}>
-          {isLogin ? <Login /> : <Registration />}
+          <Routes>
+            <Route path="login" element={<Login />} />
+            <Route path="registration" element={<Registration />} />
+            <Route path="*" element={<Navigate to="login" />} />
+          </Routes>
         </Grid>
       </Grid>
     </Container>
