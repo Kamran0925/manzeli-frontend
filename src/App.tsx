06d89fc565ffb59f5507297cb8eb35e29631d305
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-09 18:42:38 +0500

    Rendering the properties management layout page in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Layout from "./components/properties/layout/Layout";
 import { FormProvider } from "./context/FormContext";
-import Authentication from "./components/authentication/authentication";
+// import Authentication from "./components/authentication/authentication";
 
 function App() {
   return (
     <FormProvider>
-      <Authentication />
+      <Layout />
     </FormProvider>
   );
 }
