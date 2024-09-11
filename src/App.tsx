f7823975a967d721b26861fe164f4168b5975c34
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-11 17:26:06 +0500

    Add dashboard route to render property management layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import Layout from "./components/properties/layout/Layout";
 import { FormProvider } from "./context/FormContext";
-// import Authentication from "./components/authentication/authentication";
+import Authentication from "./components/authentication/authentication";
 
 function App() {
   return (
     <FormProvider>
-      <Layout />
+      <Authentication />
     </FormProvider>
   );
 }
