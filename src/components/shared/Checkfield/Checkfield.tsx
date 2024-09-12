498443efda1849819be84938fe950c8868241383
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-13 02:45:43 +0500

    Add customized check field component

---
+++ src/components/shared/Checkfield/Checkfield.tsx
@@ -0,0 +1,30 @@
+import Checkbox, { CheckboxProps } from "@mui/material/Checkbox";
+import { styled } from "@mui/material/styles";
+
+const CheckIcon = styled("span")(() => ({
+  width: "15px",
+  height: "15px",
+  borderRadius: "4px",
+  outline: "1px solid #E3E3E3",
+  "input:hover ~ &": {
+    backgroundColor: "#ebf1f5",
+  },
+}));
+
+const CheckedIcon = styled(CheckIcon)({
+  backgroundColor: "#001283",
+  outline: "1px solid #001283",
+});
+
+export const Checkfield = (props: CheckboxProps) => {
+  return (
+    <Checkbox
+      disableRipple
+      color="primary"
+      checkedIcon={<CheckedIcon />}
+      icon={<CheckIcon />}
+      sx={{ padding: 0, marginRight: "6px" }}
+      {...props}
+    />
+  );
+};
