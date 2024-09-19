9ffd2714c709b2443dd1886a34ff36620b659130
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-19 16:26:47 +0500

    Add code to render list or grid view as per gridView option

--- src/components/properties/layout/MainContent/PropertyListings/PropertyListings.tsx
+++ src/components/properties/layout/MainContent/PropertyListings/PropertyListings.tsx
@@ -1,20 +1,31 @@
 import React from "react";
 import { Property } from "./properties";
 import { Box } from "@mui/material";
+import { PropertyCardItem } from "./PropertyCardItem/PropertyCardItem";
 import { PropertyListItem } from "./PropertyListItem/PropertyListItem";
 import styles from "./PropertyListings.module.css";
 
 interface PropertyListingsProps {
   properties: Property[];
+  gridView: boolean;
 }
 
-const PropertyListings: React.FC<PropertyListingsProps> = ({ properties }) => {
+const PropertyListings: React.FC<PropertyListingsProps> = ({
+  properties,
+  gridView = false,
+}) => {
   return (
-    <Box className={styles.container}>
-      {properties.map(property => (
-        <PropertyListItem key={property.id} property={property} />
-      ))}
-    </Box>
+    <>
+      <Box className={styles.container}>
+        {properties.map(property =>
+          gridView ? (
+            <PropertyCardItem key={property.id} property={property} />
+          ) : (
+            <PropertyListItem key={property.id} property={property} />
+          ),
+        )}
+      </Box>
+    </>
   );
 };
 
