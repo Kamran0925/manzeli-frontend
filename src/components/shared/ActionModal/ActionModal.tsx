9c51241255f26d688522d104421acaaec3a41df0
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-26 17:27:44 +0500

    Remove reduntant styling for action modal

--- src/components/shared/ActionModal/ActionModal.tsx
+++ src/components/shared/ActionModal/ActionModal.tsx
@@ -7,6 +7,7 @@ import {
   Button,
 } from "@mui/material";
 import FillCross from "../../../assets/icons/ui/FillCross";
+import classNames from "classnames";
 import styles from "./ActionModal.module.css";
 
 interface ActionModalProps {
@@ -36,8 +37,6 @@ const ActionModal: React.FC<ActionModalProps> = ({
           margin: 0,
           width: "600px",
           borderRadius: "12px",
-          boxShadow:
-            "0px 20px 25px -8px rgba(0, 0, 0, 0.06),0px 8px 10px -6px rgba(0, 0, 0, 0.08), 0px 0px 0px 1px rgba(0, 0, 0, 0.06)",
         },
       }}
     >
@@ -51,15 +50,15 @@ const ActionModal: React.FC<ActionModalProps> = ({
       <DialogActions className={styles.actions}>
         <Button
           onClick={onClose}
-          variant="contained"
-          className={styles.cancelBtn}
+          variant="outlined"
+          className={classNames(styles.btn, styles.cancelBtn)}
         >
           Cancel
         </Button>
         <Button
           onClick={onDelete}
           variant="contained"
-          className={styles.deleteBtn}
+          className={classNames(styles.btn, styles.deleteBtn)}
         >
           {actionBtnText}
         </Button>
