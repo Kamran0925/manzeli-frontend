667a4574bb779f16f0766d3c6e2f1452e1a80698
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-09-26 12:23:58 +0500

    Add code logic to render registration pages as per selected account type

--- src/components/authentication/Registration/Registration.tsx
+++ src/components/authentication/Registration/Registration.tsx
@@ -1,42 +1,55 @@
-import React, { useEffect, useState } from "react";
-import { useFormContext } from "../../../context/FormContext";
+import { useEffect, useState } from "react";
+import { AccountType, useFormContext } from "../../../context/FormContext";
 import Landing from "../Landing/Landing";
 import AccountSelectable from "../AccountSelectable/AccountSelectable";
 import SignUp from "../SignUp/SignUp";
 import ContactDetails from "../ContactDetails/ContactDetails";
 import UploadImage from "../UploadImage/UploadImage";
-import Plans from "../../registration/Plans/Plans";
+import PropertyManagementFirm from "../../registration/PropertyManagementFirm/PropertyManagementFirm";
 
 const Registration = () => {
   const { formState } = useFormContext();
   const { step } = formState;
 
-  const [content, setContent] = useState(<AccountSelectable />);
+  const [content, setContent] = useState(<Landing />);
 
   useEffect(() => {
-    switch (step) {
-      case 0:
-        setContent(<Landing />);
-        break;
-      case 1:
-        setContent(<AccountSelectable />);
-        break;
-      case 2:
-        setContent(<SignUp />);
-        break;
-      case 3:
-        setContent(<ContactDetails />);
-        break;
-      case 4:
-        setContent(<UploadImage />);
-        break;
-      case 5:
-        setContent(<Plans />);
-        break;
-      default:
-        setContent(<Landing />);
-    }
-  }, [step]);
+    const renderContent = () => {
+      if (step === 0) return <Landing />;
+      if (step === 1) return <AccountSelectable />;
+
+      const isIndividual =
+        formState.accountType === AccountType.IndividualPropertyOwner;
+      const isManagementFirm =
+        formState.accountType === AccountType.PropertyManagementFirm;
+
+      if (isIndividual) {
+        switch (step) {
+          case 2:
+            return <SignUp />;
+          case 3:
+            return <ContactDetails />;
+          case 4:
+            return <UploadImage />;
+          default:
+            return <Landing />;
+        }
+      } else if (isManagementFirm) {
+        switch (step) {
+          case 2:
+            return <PropertyManagementFirm />;
+          case 3:
+            return <SignUp />;
+          default:
+            return <Landing />;
+        }
+      }
+
+      return <Landing />;
+    };
+
+    setContent(renderContent());
+  }, [formState.accountType, step]);
 
   return <div>{content}</div>;
 };
