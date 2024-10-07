cc300ca4c06517dbfd92575caa32246e1f36f91e
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-07 18:25:15 +0500

    Add constants for client registeration  account types

--- src/components/shared/AccountTypes/AccountTypes.ts
+++ src/components/shared/AccountTypes/AccountTypes.ts
@@ -1,3 +1,5 @@
+import { PROPERTY_MANAGEMENT_FIRM, INDIVIDUAL_PROPERTY_OWNER } from "./Clients";
+
 type AccountType = {
   title: string;
   description: string;
@@ -5,13 +7,18 @@ type AccountType = {
 
 export const accountTypes: AccountType[] = [
   {
-    title: "Property Management Firm",
+    title: PROPERTY_MANAGEMENT_FIRM,
     description:
       "Register as a property management company to oversee multiple properties and streamline your business operations",
   },
   {
-    title: "Individual Property Owner",
+    title: INDIVIDUAL_PROPERTY_OWNER,
     description:
       "Register as a landlord to manage your individual rental properties efficiently.",
   },
 ];
+
+export const clientTypes: { [key: string]: string } = {
+  [INDIVIDUAL_PROPERTY_OWNER]: "010",
+  [PROPERTY_MANAGEMENT_FIRM]: "020",
+};
