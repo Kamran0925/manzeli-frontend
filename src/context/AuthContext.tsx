1776c7bcb12a56e5f03ce08018944abbddd976f3
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-08 11:57:24 +0500

    Add login method to auth context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,8 +1,15 @@
-import React, { createContext, ReactNode } from "react";
-import { clientRegisteration, RegisterationData } from "../api/authApi";
+import React, { createContext, ReactNode, useState } from "react";
+import {
+  clientLogin,
+  clientRegisteration,
+  LoginData,
+  RegisterationData,
+} from "../api/authApi";
 
 interface AuthContextType {
   register: (data: RegisterationData) => Promise<void>;
+  login: (data: LoginData) => any;
+  isAuthenticated: boolean;
 }
 
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -10,12 +17,29 @@ const AuthContext = createContext<AuthContextType | undefined>(undefined);
 export const AuthProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [tokens, setTokens] = useState<{
+    access: string;
+    refresh: string;
+  } | null>(null);
+
   const register = async (data: RegisterationData) => {
     return clientRegisteration(data);
   };
 
+  const login = async (data: LoginData) => {
+    const response = await clientLogin(data);
+    if (response.access && response.refresh) {
+      setTokens({ access: response.access, refresh: response.refresh });
+      setIsAuthenticated(true);
+    }
+    return response;
+  };
+
   return (
-    <AuthContext.Provider value={{ register }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ register, login, isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
   );
 };
 
