5e637a9d6b903688b3ba7cce8a9005354771f7a2
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-15 13:18:08 +0500

    Remove create form data generic method

--- src/utils/inputFIeldHelper.ts
+++ src/utils/inputFIeldHelper.ts
@@ -1,5 +1,3 @@
-import { RegisterationData } from "../api/authApi";
-
 export const getInputType = (
   type: "text" | "password" | "email",
   showPassword: boolean,
@@ -9,22 +7,3 @@ export const getInputType = (
   }
   return type;
 };
-
-export function createFormData(data: RegisterationData): FormData {
-  const formData = new FormData();
-
-  for (const key in data) {
-    if (data.hasOwnProperty(key)) {
-      const value = data[key];
-      if (value !== undefined && value !== null) {
-        if (typeof value === "string" || typeof value === "number") {
-          formData.append(key, String(value));
-        } else if (value instanceof Blob) {
-          formData.append(key, value);
-        }
-      }
-    }
-  }
-
-  return formData;
-}
