e1ee00b6ed1cd1e01ee2ffd8947ade3e6387781b
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-15 17:13:44 +0500

    Add interface for plans data

--- src/components/common/data/planTypes.ts
+++ src/components/common/data/planTypes.ts
@@ -1,43 +1,42 @@
-interface Plan {
-  price: number;
-  duration: string;
-  type: string;
-  description: string;
-  features: string[];
+export interface Plan {
+  id: number;
+  name: string;
+  price: string;
+  billing_cycle: string;
+  memo: string;
+  created_at: string;
+  updated_at: string;
 }
+
+//This will be removed once plans data comes from backend api
 
 const plans: Plan[] = [
   {
-    price: 19,
-    duration: "monthly",
-    type: "Starter",
-    description: "Unleash the power of automation.",
-    features: ["Multi-step Zaps", "3 Premium Apps", "2 Users team"],
+    id: 1,
+    name: "Starter",
+    price: "19.00",
+    billing_cycle: "010",
+    memo: "Unleash the power of automation",
+    created_at: "2024-10-09T12:29:06Z",
+    updated_at: "2024-10-09T12:29:06Z",
   },
   {
-    price: 54,
-    duration: "monthly",
-    type: "Professional",
-    description: "Advanced tools to take your work to the next level.",
-    features: [
-      "Multi-step Zaps",
-      "Unlimited Premium Apps",
-      "50 Users team",
-      "Shared Workspace",
-    ],
+    id: 2,
+    name: "Professional",
+    price: "54.00",
+    billing_cycle: "010",
+    memo: "Advanced tools to take your work to the next level.",
+    created_at: "2024-10-09T12:30:20Z",
+    updated_at: "2024-10-09T12:30:20Z",
   },
   {
-    price: 89,
-    duration: "monthly",
-    type: "Company",
-    description: "Automation plus enterprise-grade features.",
-    features: [
-      "Multi-step Zap",
-      "Unlimited Premium Apps",
-      "Unlimited Users Team",
-      "Advanced Admin",
-      "Custom Data Retention",
-    ],
+    id: 3,
+    name: "Company",
+    price: "89.00",
+    billing_cycle: "010",
+    memo: "Automation plus enterprise grade features.",
+    created_at: "2024-10-09T12:30:54Z",
+    updated_at: "2024-10-09T12:30:54Z",
   },
 ];
 
