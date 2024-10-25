45eb36a9320bfcc4df8b0fc2ab577eda1179307c
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-25 19:30:13 +0500

    Add white background for content on settings page only

--- src/components/layout/Main/Main.tsx
+++ src/components/layout/Main/Main.tsx
@@ -1,10 +1,18 @@
 import { Box } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import styles from "./Main.module.css";
 
 const Main = () => {
+  const location = useLocation();
+  const isSettingsRoute = location.pathname === "/settings";
+
   return (
-    <Box className={styles.mainContent}>
+    <Box
+      className={styles.mainContent}
+      sx={{
+        backgroundColor: isSettingsRoute ? "#FFF" : "transparent",
+      }}
+    >
       <Outlet />
     </Box>
   );
