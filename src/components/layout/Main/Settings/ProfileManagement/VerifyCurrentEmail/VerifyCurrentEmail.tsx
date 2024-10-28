e53ce72cf8bceda820baa67d80aaab3c085e9bc8
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-28 22:57:25 +0500

    Add component to verify current user email

---
+++ src/components/layout/Main/Settings/ProfileManagement/VerifyCurrentEmail/VerifyCurrentEmail.tsx
@@ -0,0 +1,60 @@
+import { Typography, Box, Link as MuiLink } from "@mui/material";
+import { Link } from "react-router-dom";
+import InputField from "../../../../../shared/InputField/InputField";
+import classNames from "classnames";
+import styles from "./VerifyCurrentEmail.module.css";
+
+const VerifyCurrentEmail = () => {
+  return (
+    <Box className={styles.section}>
+      <Typography className={styles.infoText}>
+        We need to confirm your current email before making any changes.
+      </Typography>
+
+      <Box>
+        <Box className={styles.field}>
+          <Typography className={styles.label}>Email address</Typography>
+          <InputField
+            name="email"
+            type="email"
+            placeholder="Enter email address"
+            value=""
+            errorMessage=""
+            handleChange={() => null}
+          />
+        </Box>
+
+        <Box className={classNames(styles.field, styles.spacing1)}>
+          <Typography className={styles.label}>Password</Typography>
+          <InputField
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value=""
+            errorMessage=""
+            handleChange={() => null}
+          />
+        </Box>
+      </Box>
+
+      <Typography className={styles.resendText}>
+        Didn't receive the email?{" "}
+        <MuiLink
+          component={Link}
+          to="/resend-email"
+          color="primary"
+          sx={{
+            textDecoration: "none",
+            "&:hover": {
+              textDecoration: "underline",
+            },
+          }}
+        >
+          Resend in 30s
+        </MuiLink>
+      </Typography>
+    </Box>
+  );
+};
+
+export default VerifyCurrentEmail;
