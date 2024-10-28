84c400739c0a58fa6d6f3a6d3af327d5f656cebd
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-10-28 22:59:44 +0500

    Add component to change user email

---
+++ src/components/layout/Main/Settings/ProfileManagement/UpdateEmail/UpdateEmail.tsx
@@ -0,0 +1,42 @@
+import { Typography, Box } from "@mui/material";
+import InputField from "../../../../../shared/InputField/InputField";
+import classNames from "classnames";
+import styles from "./UpdateEmail.module.css";
+
+const UpdateEmail = () => {
+  return (
+    <Box className={styles.section}>
+      <Typography variant="h4" className={styles.infoText}>
+        Enter the new email address you want to use.
+      </Typography>
+
+      <Box>
+        <Box className={styles.field}>
+          <Typography className={styles.label}>New Email</Typography>
+          <InputField
+            name="email"
+            type="email"
+            placeholder="Enter new email address"
+            value=""
+            errorMessage=""
+            handleChange={() => null}
+          />
+        </Box>
+
+        <Box className={classNames(styles.field, styles.spacing1)}>
+          <Typography className={styles.label}>Password</Typography>
+          <InputField
+            type="password"
+            name="password"
+            placeholder="Your password"
+            value=""
+            errorMessage=""
+            handleChange={() => null}
+          />
+        </Box>
+      </Box>
+    </Box>
+  );
+};
+
+export default UpdateEmail;
