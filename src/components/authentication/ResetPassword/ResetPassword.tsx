65fb02b31eac7ade51aaf2d6724285ef2c67958e
Kamran Rizwan <kamranrizwan0925@gmail.com>
2024-11-01 20:19:33 +0500

    Add logic to get uid and token from url for reset password

--- src/components/authentication/ResetPassword/ResetPassword.tsx
+++ src/components/authentication/ResetPassword/ResetPassword.tsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
+import { useLocation } from "react-router-dom";
 import ResetPasswordSuccess from "./ResetPasswordSuccess/ResetPasswordSuccess";
 import ResetPasswordForm from "./ResetPasswordForm/ResetPasswordForm";
 
 const ResetPassword = () => {
   const [step, setStep] = useState(0);
+  const location = useLocation();
+
+  const queryParams = new URLSearchParams(location.search);
+  const uid = queryParams.get("uid");
+  const token = queryParams.get("token");
 
   const handleNext = () => {
     setStep(prevStep => Math.min(prevStep + 1, 4));
@@ -14,7 +20,9 @@ const ResetPassword = () => {
       case 1:
         return <ResetPasswordSuccess />;
       default:
-        return <ResetPasswordForm onNext={handleNext} />;
+        return (
+          <ResetPasswordForm uid={uid} token={token} onNext={handleNext} />
+        );
     }
   };
 
