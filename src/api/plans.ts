522dc62b7c5b97470366e80c82514a922032b5d8
Kamran0925 <kamranrizwan0925@gmail.com>
2025-06-20 21:34:58 +0500

    Add test plans to show on Manzeli frontend

--- src/api/plans.ts
+++ src/api/plans.ts
@@ -1,23 +1,25 @@
-import apiClient from "./apiClient";
+// import apiClient from "./apiClient";
+import { subscriptionPlansData } from "./fakeApiResponses";
 
-const SUBSCRIPTION_PLANS_URL = "/api/platform-admin/subscription-plans/";
+// const SUBSCRIPTION_PLANS_URL = "/api/platform-admin/subscription-plans/";
 
 export const getSubscriptionPlans = async () => {
-  const originalAuthHeader = apiClient.defaults.headers.common["Authorization"];
+  // const originalAuthHeader = apiClient.defaults.headers.common["Authorization"];
 
   try {
-    delete apiClient.defaults.headers.common["Authorization"];
+    // delete apiClient.defaults.headers.common["Authorization"];
 
-    const response = await apiClient.get(SUBSCRIPTION_PLANS_URL);
+    // const response = await apiClient.get(SUBSCRIPTION_PLANS_URL);
+    const response = subscriptionPlansData;
     return response.data;
   } catch (error) {
     console.error("Error fetching subscription plans:", error);
     throw error;
   } finally {
-    if (originalAuthHeader) {
-      apiClient.defaults.headers.common["Authorization"] = originalAuthHeader;
-    } else {
-      delete apiClient.defaults.headers.common["Authorization"];
-    }
+    // if (originalAuthHeader) {
+    //   apiClient.defaults.headers.common["Authorization"] = originalAuthHeader;
+    // } else {
+    //   delete apiClient.defaults.headers.common["Authorization"];
+    // }
   }
 };
