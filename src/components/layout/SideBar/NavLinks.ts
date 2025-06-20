2579d30372032aa2ee3754f86419665c31de54e1
Kamran0925 <kamranrizwan0925@gmail.com>
2025-06-20 19:17:11 +0500

    Comment unused sidenav links for manzeli-frontend

--- src/components/layout/SideBar/NavLinks.ts
+++ src/components/layout/SideBar/NavLinks.ts
@@ -1,37 +1,37 @@
-import Grid from "../../../assets/icons/ui/Grid";
+// import Grid from "../../../assets/icons/ui/Grid";
 import Home from "../../../assets/icons/ui/Home";
-import Message from "../../../assets/icons/ui/Message";
-import Unit from "../../../assets/icons/ui/Unit";
+// import Message from "../../../assets/icons/ui/Message";
+// import Unit from "../../../assets/icons/ui/Unit";
 import Tenant from "../../../assets/icons/ui/Tenant";
 import Plan from "../../../assets/icons/ui/Plan";
 import Settings from "../../../assets/icons/ui/Settings";
 
 export const NAV_LINKS = [
-  {
-    route: "/dashboard",
-    icon: Grid,
-    label: "Dashboard",
-  },
+  // {
+  //   route: "/dashboard",
+  //   icon: Grid,
+  //   label: "Dashboard",
+  // },
   {
     route: "/property/listings",
     icon: Home,
     label: "Properties",
   },
-  {
-    route: "/units",
-    icon: Unit,
-    label: "Units",
-  },
+  // {
+  //   route: "/units",
+  //   icon: Unit,
+  //   label: "Units",
+  // },
   {
     route: "/tenancy",
     icon: Tenant,
     label: "Tenancy Contracts",
   },
-  {
-    route: "/messages",
-    icon: Message,
-    label: "Messages",
-  },
+  // {
+  //   route: "/messages",
+  //   icon: Message,
+  //   label: "Messages",
+  // },
   {
     route: "/pricing",
     icon: Plan,
